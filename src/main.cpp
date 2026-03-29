4406c0285d551ab19bf6f027ba1621364e7e302a
DMS564 <austin.d.leake@gmail.com>
2026-03-28 21:18:34 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,136 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PauseLayer.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+class $modify(ZoomPauseLayer, PauseLayer) {
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+    struct Fields {
+        float m_currentZoom = 1.0f;
+        float m_targetZoom = 1.0f;
+        bool m_isPanning = false;
+        CCPoint m_lastMousePos = CCPointZero;
+        CCPoint m_panOffset = CCPointZero;
+    };
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+    bool init(bool p0) {
+        if (!PauseLayer::init(p0)) return false;
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+        m_fields->m_currentZoom = 1.0f;
+        m_fields->m_targetZoom = 1.0f;
+        m_fields->m_panOffset = CCPointZero;
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+        this->setKeypadEnabled(true);
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+        // schedule update for smooth zoom
+        if (Mod::get()->getSettingValue<bool>("smooth-zoom")) {
+            this->schedule(schedule_selector(ZoomPauseLayer::smoothZoomUpdate));
+        }
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+    void scrollWheel(float dy, float dx) {
+        float zoomSpeed = Mod::get()->getSettingValue<double>("zoom-speed");
+        float maxZoom = Mod::get()->getSettingValue<double>("max-zoom");
+        float minZoom = Mod::get()->getSettingValue<double>("min-zoom");
+
+        // dy > 0 means scroll up = zoom in, dy < 0 means scroll down = zoom out
+        float zoomDelta = dy > 0 ? zoomSpeed : -zoomSpeed;
+        m_fields->m_targetZoom = clampf(
+            m_fields->m_targetZoom + zoomDelta,
+            minZoom,
+            maxZoom
+        );
+
+        if (!Mod::get()->getSettingValue<bool>("smooth-zoom")) {
+            // instant zoom
+            m_fields->m_currentZoom = m_fields->m_targetZoom;
+            applyZoom();
+        }
+    }
+
+    void smoothZoomUpdate(float dt) {
+        if (std::abs(m_fields->m_currentZoom - m_fields->m_targetZoom) > 0.001f) {
+            // lerp towards target
+            float lerpFactor = clampf(dt * 12.0f, 0.0f, 1.0f);
+            m_fields->m_currentZoom += (m_fields->m_targetZoom - m_fields->m_currentZoom) * lerpFactor;
+            applyZoom();
+        }
+    }
+
+    void applyZoom() {
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        CCPoint center = ccp(winSize.width / 2.0f, winSize.height / 2.0f);
+
+        this->setScale(m_fields->m_currentZoom);
+
+        // keep the zoom centered with pan offset
+        CCPoint anchoredPos = ccp(
+            center.x - (center.x * m_fields->m_currentZoom) + m_fields->m_panOffset.x,
+            center.y - (center.y * m_fields->m_currentZoom) + m_fields->m_panOffset.y
+        );
+        this->setPosition(anchoredPos);
+    }
+
+    bool ccTouchBegan(CCTouch* touch, CCEvent* event) {
+        // middle mouse / right click for panning when zoomed in
+        if (m_fields->m_currentZoom > 1.05f) {
+            m_fields->m_isPanning = true;
+            m_fields->m_lastMousePos = touch->getLocation();
+        }
+        return PauseLayer::ccTouchBegan(touch, event);
+    }
+
+    void ccTouchMoved(CCTouch* touch, CCEvent* event) {
+        if (m_fields->m_isPanning && m_fields->m_currentZoom > 1.05f) {
+            CCPoint currentPos = touch->getLocation();
+            CCPoint delta = ccpSub(currentPos, m_fields->m_lastMousePos);
+            m_fields->m_panOffset = ccpAdd(m_fields->m_panOffset, delta);
+            m_fields->m_lastMousePos = currentPos;
+            applyZoom();
+        }
+        PauseLayer::ccTouchMoved(touch, event);
+    }
+
+    void ccTouchEnded(CCTouch* touch, CCEvent* event) {
+        m_fields->m_isPanning = false;
+        PauseLayer::ccTouchEnded(touch, event);
+    }
+
+    void onQuit(CCObject* sender) {
+        resetZoom();
+        PauseLayer::onQuit(sender);
+    }
+
+    void onResume(CCObject* sender) {
+        resetZoom();
+        PauseLayer::onResume(sender);
+    }
+
+    void onPracticeMode(CCObject* sender) {
+        resetZoom();
+        PauseLayer::onPracticeMode(sender);
+    }
+
+    void keyDown(enumKeyCodes key) {
+        // reset zoom with R key
+        if (key == enumKeyCodes::KEY_R) {
+            m_fields->m_targetZoom = 1.0f;
+            m_fields->m_currentZoom = 1.0f;
+            m_fields->m_panOffset = CCPointZero;
+            applyZoom();
+            return;
+        }
+        PauseLayer::keyDown(key);
+    }
+
+    void resetZoom() {
+        if (Mod::get()->getSettingValue<bool>("reset-on-close")) {
+            m_fields->m_targetZoom = 1.0f;
+            m_fields->m_currentZoom = 1.0f;
+            m_fields->m_panOffset = CCPointZero;
+            applyZoom();
+        }
+    }
 };
